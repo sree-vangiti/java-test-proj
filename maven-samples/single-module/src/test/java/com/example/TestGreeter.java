c11e9829054229d1dded5216c026122e568e8996
srikanth <sree.vangiti@gmail.com>
2020-01-13 12:59:52 +0000

    change in tst code-srikanth2

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -37,11 +37,4 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
-
-    @Test
-  public void greetShouldIncludeGreetingMessage-by-srikanth() {
-    String someone = "World, Thanks you making me Happy- by srikanth";
-
-    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
-  }
 }
